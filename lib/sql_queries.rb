53a8095aa90917540e41b66998caff8c0a22af7c
Michael Waide <mvwaide@gmail.com>
2020-08-10 17:23:52 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-"SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1;"
+"SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
